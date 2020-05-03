94aa2db108c0705a65c9106c28da0504c5ffd62c
huhn511 <sehe89@gmail.com>
2020-05-03 19:16:11 +0200

    add proper script

--- script.py
+++ script.py
@@ -1,9 +1,27 @@
-print("Hello world from 'script.py'!")
 
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM) # GPIO Nummern statt Board Nummern
- 
+import time
+
 RELAIS_1_GPIO = 17
+
+
+GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Modus zuweisen
-GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # aus
-GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # an
+
+# Initial state for LEDs:
+print("Testing RF out, Press CTRL+C to exit")
+
+try:
+    print("set GIOP high")
+    GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # an              
+    time.sleep(5) 
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+   print("Keyboard interrupt")
+
+except:
+   print("some error") 
+
+finally:
+   print("clean up") 
+   GPIO.cleanup() # cleanup all GPIO 
